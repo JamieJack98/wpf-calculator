be797b3101101272b0ed999e70c7eba591c7c40c
Jamie <jamiejack9468@outlook.com>
2026-01-12 21:06:44 +0000

    Calculator fully working

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         double lastNumber, result;
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
@@ -24,20 +25,46 @@ namespace Calculator
             acButton.Click += AcButton_Click;
             plusMinusButton.Click += plusMinusButton_Click;
             percentageButton.Click += percentageButton_Click;
-            equalsButton.Click += EqualsButton_Click;
+            equalsButton.Click += equalsButton_Click;
         }
 
-        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        private void equalsButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch(selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.subtract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.divide(lastNumber, newNumber);
+                        break;
+                }
+            }
+
+            resultLabel.Content = result.ToString();
         }
 
         private void percentageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
             {
-                lastNumber = lastNumber / 100;
-                resultLabel.Content = lastNumber.ToString();
+                tempNumber = (tempNumber / 100);
+                if(lastNumber != 0)
+                {
+                    tempNumber *= lastNumber;
+                }
+
+                resultLabel.Content = tempNumber.ToString();
             }
         }
 
@@ -53,6 +80,8 @@ namespace Calculator
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
+            result = 0;
+            lastNumber = 0;
         }
 
         private void operationButton_Click(object sender, RoutedEventArgs e)
@@ -61,41 +90,74 @@ namespace Calculator
             {
                 resultLabel.Content = "0";
             }
+
+            if (sender == multiplyButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divideButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == plusButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                selectedOperator = SelectedOperator.Subtraction;
+
         }
 
         private void numberButton_Click(object sender, RoutedEventArgs e)
         {
-            int selectedValue = 0;
-
-            if(sender == zeroButton)
-                selectedValue = 0;
-            if(sender == oneButton)
-                selectedValue = 1;
-            if (sender == twoButton)
-                selectedValue = 2;
-            if (sender == threeButton)
-                selectedValue = 3;
-            if (sender == fourButton)
-                selectedValue = 4;
-            if (sender == fiveButton)
-                selectedValue = 5;
-            if (sender == sixButton)
-                selectedValue = 6;
-            if (sender == sevenButton)
-                selectedValue = 7;
-            if (sender == eightButton)
-                selectedValue = 8;
-            if (sender == nineButton)
-                selectedValue = 9;
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
 
             if (resultLabel.Content.ToString() == "0")
+                resultLabel.Content = selectedValue.ToString();
+            else
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            
+        }
+
+        private void decimalButton_Click(Object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString().Contains("."))
             {
-                resultLabel.Content = $"{selectedValue}";
+                // Do Nothing
             }
             else
             {
-                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+                resultLabel.Content = $"{resultLabel.Content}.";
             }
         }
+        public enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+
+        public class SimpleMath
+        {
+            public static double add(double n1, double n2)
+            {
+                return n1 + n2; 
+            }
+            public static double subtract(double n1, double n2)
+            {
+                return n1 - n2; 
+            }
+            public static double multiply(double n1, double n2)
+            {
+                return n1 * n2; 
+            }
+            public static double divide(double n1, double n2)
+            {
+                if(n2 == 0)
+                {
+                    MessageBox.Show("Division by 0 is not supported", "Wrong Operation",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return 0;
+                }
+
+                return n1 / n2; 
+            }
+        }
+
     }
 }
